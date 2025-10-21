b4fe11b7411ec9b9427c74c895fdddc5fee26e82
denalisun <carvekidsak@gmail.com>
2025-10-21 06:20:51 -0800

    Pretty much done

--- Makefile
+++ Makefile
@@ -1,6 +1,16 @@
 CC=gcc
-OUT=yafl
+OUT=bin/yafl
 SRC=src/main.c
 
 build:
+	git clone https://github.com/denalisun/yafl.git
+	make -C yafl
+	mkdir bin
+	cp yafl/yafl.exe bin/yafl.exe
 	$(CC) -o $(OUT) $(SRC) -std=c99 -Wall -Werror
+
+clean:
+	rm -rf bin yafl
+
+install:
+	cp -a bin/* /usr/bin
--- src/main.c
+++ src/main.c
@@ -1,30 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+
+char* convert_path_to_windows(char* path) {
+    char* newPath = (char*)malloc(strlen(path)+3);
+    sprintf(newPath, "Z:%s", path);
+    for (int i = 0; i < strlen(newPath); i++) {
+        if (newPath[i] == '/') newPath[i] = '\\';
+    }
+    return newPath;
+}
+
+char* create_args(char *playPath, char **allTweaks, size_t allTweaksUsed, bool bIsServer) {
+    size_t len = strlen(playPath) + 7 + 1;
+    char* r = (char*)calloc(len, sizeof(char)); 
+    sprintf(r, "-p \"%s\" ", convert_path_to_windows(playPath));
+    for (size_t i = 0; i < allTweaksUsed; i++) {
+        size_t tweakLen = strlen(allTweaks[i]) + 6 + 1;
+        char* tweakArg = (char*)calloc(tweakLen, sizeof(char));
+        sprintf(tweakArg, "-t \"%s\" ", convert_path_to_windows(allTweaks[i]));
+        r = realloc(r, len + tweakLen);
+        len += tweakLen;
+        strcat(r, tweakArg);
+    }
+    if (bIsServer) {
+        size_t serverLen = 3;
+        r = realloc(r, len + serverLen);
+        strcat(r, "-s");
+    }
+    return r;
+}
 
 int main(int argc, char **argv) {
     char *playPath = NULL;
     size_t allTweaksSize = 1;
     size_t allTweaksUsed = 0;
     char **allTweaks = (char**)calloc(allTweaksSize, sizeof(char*));
+    bool bIsServer = false;
     for (int i = 0; i < argc; i++) {
         if (strcmp(argv[i], "-p") == 0 || strcmp(argv[i], "--play") == 0) {
             playPath = argv[i+1];
-        } else if (strcmp(argv[i], "-t") == 0 || strcmp(argv[i], "-tweak") == 0) {
+        } else if (strcmp(argv[i], "-t") == 0 || strcmp(argv[i], "--tweak") == 0) {
             if (allTweaksUsed == allTweaksSize) {
                 allTweaksSize++;
                 allTweaks = realloc(allTweaks, sizeof(char*) * allTweaksSize);
             }
             allTweaks[allTweaksUsed] = argv[i+1];
             allTweaksUsed++;
+        } else if (strcmp(argv[i], "-s") == 0 || strcmp(argv[i], "--server") == 0) {
+            bIsServer = true;
         }
     }
 
-    if (playPath != NULL) {
-        printf("%s\n", playPath);
-    }
-    for (int i = 0; i < allTweaksUsed; i++) {
-        printf("%d  \"%s\"\n", i, allTweaks[i]);
-    }
+    char* usedArgs = create_args(playPath, allTweaks, allTweaksUsed, bIsServer);
+    size_t len = 17 + (strlen(usedArgs) + 1);
+    char* wineCmd = (char*)calloc(len, sizeof(char));
+    sprintf(wineCmd, "wine ./yafl.exe %s", usedArgs);
+    
+    system(wineCmd);
+
     return 0;
 }
